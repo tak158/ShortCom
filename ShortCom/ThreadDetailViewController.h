d0954818386cbb7fecdb9673d93a1282dc7c336c
snthd <158rune@gmail.com>
2013-05-17 09:07:06 +0900

    画面遷移作成済み

---
+++ ShortCom/ThreadDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThreadDetailViewController.h
+//  ShortCom
+//
+//  Created by uchikawa takao on 2013/05/17.
+//  Copyright (c) 2013年 uchikawa takao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThreadDetailViewController : UIViewController
+
+@end
---
+++ ShortCom/ThreadDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  ThreadDetailViewController.m
+//  ShortCom
+//
+//  Created by uchikawa takao on 2013/05/17.
+//  Copyright (c) 2013年 uchikawa takao. All rights reserved.
+//
+
+#import "ThreadDetailViewController.h"
+
+@interface ThreadDetailViewController ()
+
+@end
+
+@implementation ThreadDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
