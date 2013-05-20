580ebffe6fe284776ecbd87f3f7cddaaf1e820b9
snthd <158rune@gmail.com>
2013-05-20 17:45:37 +0900

    Cellのデザイン変更前

---
+++ ShortCom/CommentViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CommentViewCell.h
+//  ShortCom
+//
+//  Created by uchikawa takao on 2013/05/20.
+//  Copyright (c) 2013年 uchikawa takao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentViewCell : UITableViewCell
+
+@end
---
+++ ShortCom/CommentViewCell.m
@@ -0,0 +1,29 @@
+//
+//  CommentViewCell.m
+//  ShortCom
+//
+//  Created by uchikawa takao on 2013/05/20.
+//  Copyright (c) 2013年 uchikawa takao. All rights reserved.
+//
+
+#import "CommentViewCell.h"
+
+@implementation CommentViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
