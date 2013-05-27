4842da3197ed651d5c23c5ed7badf7ca58e45809
snthd <158rune@gmail.com>
2013-05-27 10:36:46 +0900

    kari commit

---
+++ ShortCom/ngUser.h
@@ -0,0 +1,14 @@
+//
+//  ngUser.h
+//  ShortCom
+//
+//  Created by uchikawa takao on 2013/05/27.
+//  Copyright (c) 2013年 uchikawa takao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ngUser : NSObject
+@property (strong, nonatomic) NSNumber* originUser;
+@property (strong, nonatomic) NSNumber* targetUser;
+@end
---
+++ ShortCom/ngUser.m
@@ -0,0 +1,13 @@
+//
+//  ngUser.m
+//  ShortCom
+//
+//  Created by uchikawa takao on 2013/05/27.
+//  Copyright (c) 2013年 uchikawa takao. All rights reserved.
+//
+
+#import "ngUser.h"
+
+@implementation ngUser
+
+@end
